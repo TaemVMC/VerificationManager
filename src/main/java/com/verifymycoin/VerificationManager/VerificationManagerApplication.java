b9b90d66aa171c7603bf2542a6f556c966e25cd1
taehoonhwang <on91716@gmail.com>
2021-09-14 03:00:06 +0900

    feat:Spring boot 프로젝트 초기화
    - 종속 패키지
    	- Spring Cloud Config Client
    	- Spring Web
    	- Spring Boot DevTools
    	- Lombok
    	- Spring Boot Actuator

---
+++ src/main/java/com/verifymycoin/VerificationManager/VerificationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.verifymycoin.VerificationManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verifymycoin/VerificationManager/VerificationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verifymycoin.VerificationManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
